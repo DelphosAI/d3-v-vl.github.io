4867dab6f6fc64920088fd0cd7e87747fc61cd47
Prince Takyi Akomea <prince.akomea.stu@uenr.edu.gh>
2024-07-25 09:07:56 +0000

    africa map

--- three/ha.js
+++ three/ha.js
@@ -10,14 +10,20 @@
 
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+
+
 
 
 window.THREE = THREE;
 
+// setup scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#218bbf");
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color("#131313");
+
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -25,6 +31,167 @@ new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 3
 
+
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+document.body.appendChild(labelRenderer.domElement);
+
+const controls = new OrbitControls(camera, labelRenderer.domElement);
+controls.minDistance = 5;
+controls.maxDistance = 100;
+
+// create a plane group
+const planeGroup = new THREE.Group();
+
+scene.add(planeGroup);
+
+// create bar group
+const barGroup = new THREE.Group();
+scene.add(barGroup);
+
+// draw a plane to plot the data points
+const planeGeometry = new THREE.PlaneGeometry(3, 3);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: "#D8E2DC", side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// plane.rotation.x = -1.2;
+plane.rotation.x = -Math.PI / 2; // Rotate plane to be horizontal
+plane.position.y = -0.2;
+// planeGroup.add(plane);
+
+// add axes helper
+const axesHelper = new THREE.AxesHelper( 5 );
+// scene.add( axesHelper );
+
+// draw test box
+const geometry = new THREE.BoxGeometry(0.1, 2, 0.1);
+
+const material = new THREE.MeshBasicMaterial({ color: "#9D8189", wireframe: false });
+const cube = new THREE.Mesh(geometry, material);
+cube.position.y = -0.1;
+cube.position.x = -1.2;
+cube.position.z = 1;
+// barGroup.add(cube);
+
+// create geo group
+const geoGroup = new THREE.Group();
+geoGroup.scale.set(5, 5, 3);
+geoGroup.position.y = 2;
+geoGroup.position.z = 1;
+geoGroup.position.x = -1;
+geoGroup.rotation.x = 1;
+scene.add(geoGroup);
+
+
+// Convert latitude and longitude to plane coordinates
+const latLonToVector3 = (lat, lon) => {
+    const x = (lon + 180) * (3 / 360) - 1.5; // Normalize longitude to plane width
+    const y = -(lat - 90) * (3 / 180) -1;  // Normalize latitude to plane height
+    return new THREE.Vector3(x, 0, y);
+};
+
+const createCountryBorders = (coordinates) => {
+    const shapeVertices = coordinates[0].map(([lon, lat]) =>
+        latLonToVector3(lat, lon)
+    );
+
+    return new THREE.BufferGeometry().setFromPoints(shapeVertices);
+};
+
+const createBordersLineString = (coordinates) => {
+    const shapeVertices = coordinates.map(([lon, lat]) =>
+        latLonToVector3(lat, lon)
+    );
+
+    return new THREE.BufferGeometry().setFromPoints(shapeVertices);
+
+}
+
+fetch("./assets/africa.geojson")
+    .then((response) => response.json())
+    .then((data) => {
+        data.features.flatMap((feature) => {
+            const { type, coordinates } = feature.geometry;
+            const countryName = feature.properties.name;
+
+            let color = "#ac1e44"; // Default color
+           if (countryName === "Eastern Africa") {
+                color = "#ef4444";
+           }
+           else if (countryName === "Northern Africa") {
+                color = "#f59e0b";
+           }
+              else if (countryName === "Southern Africa") {
+                color = "#10b981";
+           }
+                else if (countryName === "Western Africa") {
+                color = "#3b82f6";
+           }
+                else if (countryName === "Central Africa") {
+                color = "#00acc1";
+           }
+
+            if (type === "Polygon") {
+                const geometry = createCountryBorders(coordinates);
+                const material = new THREE.LineBasicMaterial({ color });
+                const line = new THREE.Line(geometry, material);
+
+                geoGroup.add(line);
+            } else if (type === "MultiPolygon") {
+                coordinates.forEach((polygon) => {
+                    const geometry = createCountryBorders(polygon);
+                    const material = new THREE.LineBasicMaterial({ color });
+                    const line = new THREE.Line(geometry, material);
+                    geoGroup.add(line);
+                });
+            }
+            else if (type === "LineString") {
+                const geometry = createBordersLineString(coordinates);
+                const material = new THREE.LineBasicMaterial({ color });
+                const line = new THREE.Line(geometry, material);
+
+                // add country name
+                const earthDiv = document.createElement('div');
+                earthDiv.className = 'label';
+                earthDiv.textContent = countryName;
+                //earthDiv.style.marginTop = '-1em';
+                const earthLabel = new CSS2DObject(earthDiv);
+
+                if (countryName === "Northern Africa") {
+                    earthLabel.position.set(0.04, 0, 0.02);
+                }
+                else if (countryName === "Eastern Africa") {
+                    earthLabel.position.set(0.32, 0, 0.45);
+                }
+                else if (countryName === "Southern Africa") {
+                    earthLabel.position.set(0.19, 0, 0.92);
+                }
+                else if (countryName === "Western Africa") {
+                    earthLabel.position.set(0.01, 0, 0.26);
+                }
+                else if (countryName === "Central Africa") {
+                    earthLabel.position.set(0.17, 0, 0.5);
+                }
+                line.add(earthLabel);
+                earthLabel.layers.set(0);
+
+                geoGroup.add(line);
+
+
+            }
+            else {
+                console.warn("Unsupported geometry type:", type);
+            }
+
+
+        });
+    });
+
+
+// Set camera position and render the scene
+camera.position.set(0, 3, 5);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 const data = 'assets/africa_data_summarized.csv';
 let processedData = [];
@@ -53,10 +220,6 @@ Papa.parse(data, {
 
     }
 });
-
-let selectedRegion = '';
-let selectedLender = '';
-let selectedSector = '';
 
 function getOptions(region, option = "lender") {
     const options = new Set();
@@ -97,31 +260,23 @@ regionSelect.addEventListener('change', (e) => {
 });
 
 
-lenderSelect.addEventListener('change', (e) => {
-    selectedLender = e.target.value;
-});
-
-sectorSelect.addEventListener('change', (e) => {
-    selectedSector = e.target.value;
-
-});
 
 const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
 
-function createChart(selectedRegion) {
+function createChart(selectedAfrica) {
 
     if (processedData.length === 0) {
         return;
     }
 
-    if (!selectedRegion) {
+    if (!selectedAfrica) {
         alert('Please select a region');
         return;
     }
-    let filteredData = processedData.filter(d => d.region === selectedRegion);
+    let filteredData = processedData.filter(d => d.region === selectedAfrica);
 
 
-    // processedData.filter(d => d.region === selectedRegion).forEach((d, i) => {
+    // processedData.filter(d => d.region === selectedAfrica).forEach((d, i) => {
     //     const amount = parseFloat(d.total_amount_usd);
     //     const transactions = parseInt(d.number_transactions);
     //     const x = (i % 5) * 5; // x-position based on index
@@ -159,9 +314,9 @@ function createChart(selectedRegion) {
         const z = Math.floor(i / 5) * 5; // z-position based on index
         const cube = new THREE.Mesh(geometry, material);
         cube.position.set(x, geometry.parameters.height / 2, z);
-        scene.add(cube);
+        planeGroup.add(cube);
     });
-    camera.position.set(0, 20, 30);
+   // camera.position.set(0, 20, 30);
 
 }
 
@@ -181,11 +336,22 @@ const animate = function () {
 
     // Flyover effect
     t += 0.01;
-    camera.position.x = 20 * Math.sin(t);
-    camera.position.z = 20 * Math.cos(t);
-    camera.lookAt(new THREE.Vector3(10, 0, 10));
+    //camera.position.x = 20 * Math.sin(t);
+    //camera.position.z = 20 * Math.cos(t);
+   // camera.lookAt(new THREE.Vector3(10, 0, 10));
 
     renderer.render(scene, camera);
+    labelRenderer.render(scene, camera);
+
 };
 
 animate();
+
+function handleWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    labelRenderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+window.addEventListener('resize', handleWindowResize, false);
